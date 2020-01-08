618ea7715518dea8c02256e9cd34d8d326e771ac
Heidi Joenpolvi <heidi.joenpolvi@gmail.com>
2020-01-08 09:38:18 +0200

    Web form projekti

---
+++ POsite/POsite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="POsite.Global" Language="C#" %>
---
+++ POsite/POsite/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(POsite.Startup))]
+namespace POsite
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
